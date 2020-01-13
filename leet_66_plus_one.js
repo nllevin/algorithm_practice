74e6f14cd190ac3ec1c94f0cd77912420814bf50
nllevin <50681582+nllevin@users.noreply.github.com>
2020-01-13 10:31:18 -0500

    Complete Leet 66 Plus One

---
+++ leet_66_plus_one.js
@@ -0,0 +1,35 @@
+// Given a non - empty array of digits representing a non - negative integer, plus one to the integer.
+
+// The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+// You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+//   Example 1:
+
+// Input: [1, 2, 3]
+// Output: [1, 2, 4]
+// Explanation: The array represents the integer 123.
+// Example 2:
+
+// Input: [4, 3, 2, 1]
+// Output: [4, 3, 2, 2]
+// Explanation: The array represents the integer 4321.
+
+const plusOne = (digits, end = digits.length - 1) => {
+  if (end < 0) {
+    digits.unshift(1);
+    return digits;
+  }
+
+  if (++digits[end] === 10) {
+    digits[end] = 0;
+    digits = plusOne(digits, end - 1);
+  }
+
+  return digits;
+};
+
+console.log(plusOne([1, 2, 3])); // [1, 2, 4]
+console.log(plusOne([4, 3, 2, 1])); // [4, 3, 2, 2]
+console.log(plusOne([9])); // [1, 0]
+console.log(plusOne([9, 9])); // [1, 0, 0]
